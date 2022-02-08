de85e45de41a877f7aa40cbd8df0430178518435
bizmaoun <bizmaounl2007@gmail.com>
2022-02-07 19:36:47 -0800

    set the category and product models

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Category extends Model
 {
@@ -18,6 +17,5 @@ class Category extends Model
     {
         return $this->hasMany(Product::class);
     }
-
 
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
-    use HasFactory;
+    public const TABLE = 'products';
+
+    protected $guarded = [];
+
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
 }
+
