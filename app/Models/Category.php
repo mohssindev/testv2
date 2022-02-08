78f7e6e864fd0f14cfc682935f1ff6a1881e5f96
bizmaoun <bizmaounl2007@gmail.com>
2022-02-07 19:31:05 -0800

    set the category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Product;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Category extends Model
 {
-    use HasFactory;
+    public const TABLE = 'categories';
+
+    protected $guarded = [];
+
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
+
+
 }
