36f025638a4f3dd1f3b73bf6f6493a08f6d76d10
bizmaoun <bizmaounl2007@gmail.com>
2022-02-07 17:08:10 -0800

    installation of a new laravel app with vuejs and tailwindCss.

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Product Store</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <style>
+            body {
+                font-family: 'Nunito', sans-serif;
+            }
+        </style>
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <h1 class="text-red-500 bg-orange-500">Testing</h1>
+        <div id="app">
+            <App></App>
+        </div>
+    <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
+
